f885285136474501546b57937a27fa2dab8740ab
Chanakyasinde <chanakyasinde@gmail.com>
2025-11-29 15:12:44 +0530

    fix: bug

--- frontend/App.js
+++ frontend/App.js
@@ -5,6 +5,7 @@ import {  StyleSheet } from "react-native";
 
 import AppNavigator from "./src/navigation/AppNavigator";
 import { COLORS } from "./src/utils/constants";
+import { AppProvider } from "./src/context/AppContext";
 
 export default function App() {
   return (
